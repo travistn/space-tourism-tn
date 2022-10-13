2960fb75a28eda184481fe75bc0853e1bd5bd7f6
travistn <travisn714@gmail.com>
2022-10-13 15:58:35 -0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'barlow-condensed': ['Barlow Condensed', 'sans-serif'],
+        bellefair: ['Bellefair', 'serif'],
+      },
+      colors: {
+        'color-lavendar': '#d0d6f9',
+        'color-black': '#0b0d17',
+      },
+    },
   },
   plugins: [],
 };
