51b9504a7e915a43387501f0b8b65a8fe028d544
travistn <travisn714@gmail.com>
2022-10-18 17:21:42 -0700

    Designed navbar with mobile first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,8 @@ const Navbar = () => {
     <div className='w-screen absolute flex flex-row items-center mt-8 pl-8 pr-8 md:pr-0 md:pl-12 md:h-24 md:mt-0 lg:mt-8'>
       <img src={logo} alt='logo' className='w-10 h-10 md:w-12 md:h-12' />
       <img src={menuIcon} alt='menu-icon' className='w-6 h-[21px] ml-auto md:invisible' />
-      <div className='h-[1px] w-1/3 md:hidden lg:flex bg-white opacity-25 m-4'></div>
-
-      <div className='hidden md:flex justify-center min-w-[550px] lg:min-w-[830px] h-24 items-center bg-navbar-links backdrop-blur-2xl'>
-        <div className='flex flex-row justify-center text-white uppercase font-barlow-condensed gap-16 max-w-[356px] text-sm lg:text-base '>
+      <div className='hidden md:flex justify-center min-w-[550px] lg:min-w-[830px] 2xl:min-w-[1200px] h-24 items-center bg-navbar-links backdrop-blur-2xl'>
+        <div className='flex flex-row justify-center text-white uppercase font-barlow-condensed gap-16 max-w-[356px] text-sm lg:text-base 2xl:text-xl'>
           <h5 className='flex flex-row tracking-[2.3px] lg:tracking-[3px]'>
             <span className='font-bold mr-2 md:hidden lg:flex'>00</span>Home
           </h5>
