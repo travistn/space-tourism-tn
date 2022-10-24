f4e44a0389b734608ca43498e25fd370f9a88d91
travistn <travisn714@gmail.com>
2022-10-24 16:53:30 -0700

    Added design for tablet display

--- src/containers/Destination.jsx
+++ src/containers/Destination.jsx
@@ -16,43 +16,49 @@ const Destination = () => {
   };
 
   return (
-    <div className='w-screen h-screen bg-destination-mobile bg-no-repeat bg-cover bg-center flex flex-col items-center overflow-auto'>
+    <div className='w-screen h-screen bg-destination-mobile md:bg-destination-tablet bg-no-repeat bg-cover bg-center flex flex-col items-center overflow-auto'>
       <Navbar />
-      <div className='flex flex-col m-auto items-center gap-8 mb-[2rem] mt-8'>
-        <h5 className='uppercase text-white font-barlow-condensed text-[16px] tracking-[2.7px] leading-[19px]'>
+      <div className='flex flex-col m-auto items-center gap-8 mb-[2rem] mt-8 md:mt-12'>
+        <h5 className='uppercase text-white font-barlow-condensed text-[16px] md:text-[20px] tracking-[2.7px] md-tracking[3.4px] leading-[19px] md:leading-[24px]'>
           <span className='font-bold mr-4 opacity-25'>01</span>Pick your destination
         </h5>
         <img
           src={require(`../assets/destination/image-${destination.name.toLowerCase()}.png`)}
           alt='destination-pic'
-          className='w-[170px] h-[170px]'
+          className='w-[170px] md:w-[300px] h-[170px] md:h-[300px] md:mt-4'
         />
-        <div className='flex flex-row gap-8 text-color-lavendar font-barlow-condensed text-[14px] tracking-[2.3px] uppercase'>
+        <div className='flex flex-row gap-8 text-color-lavendar font-barlow-condensed text-[14px] md:text-[16px] tracking-[2.3px] md:tracking-[2.7px] md:leading-[19px]  uppercase'>
           {destinations.map((destination) => (
             <p onClick={activeDestination} key={destination.name}>
               {destination.name}
             </p>
           ))}
         </div>
-        <h3 className='text-white font-bellefair text-[56px] leading-[64px] uppercase font-normal'>
+        <h3 className='text-white font-bellefair text-[56px] md:text-[80px] leading-[64px] md:leading-[92px] uppercase font-normal'>
           {destination.name}
         </h3>
-        <p className='w-[330px] text-color-lavendar text-[15px] font-barlow leading-[25px] text-center mt-[-1.5rem]'>
+        <p className='w-[330px] md:w-[573px] text-color-lavendar text-[15px] md:text-[16px] font-barlow leading-[25px] md:leading-[28px] text-center mt-[-1.5rem]'>
           {destination.description}
         </p>
-        <div className='w-[330px] h-[1px] bg-[#383B4B]'></div>
-        <h5 className='text-[15px] uppercase text-color-lavendar font-barlow-condensed leading-[17px] tracking-[2.3px]'>
-          Avg. Distance
-        </h5>
-        <h4 className='text-[28px] text-white font-bellefair uppercase leading-[32px] mt-[-1rem]'>
-          {destination.distance}
-        </h4>
-        <h5 className='text-[15px] uppercase text-color-lavendar font-barlow-condensed leading-[17px] tracking-[2.3px]'>
-          Est. Travel Time
-        </h5>
-        <h4 className='text-[28px] text-white font-bellefair uppercase leading-[32px] mt-[-1rem]'>
-          {destination.travel}
-        </h4>
+        <div className='w-[330px] md:w-[573px] h-[1px] bg-[#383B4B]'></div>
+        <div className='flex flex-col md:flex-row gap-8 md:gap-16'>
+          <div className='flex flex-col gap-4 items-center'>
+            <h5 className='text-[15px] uppercase text-color-lavendar font-barlow-condensed leading-[17px] tracking-[2.3px]'>
+              Avg. Distance
+            </h5>
+            <h4 className='text-[28px] text-white font-bellefair uppercase leading-[32px] '>
+              {destination.distance}
+            </h4>
+          </div>
+          <div className='flex flex-col gap-4 items-center'>
+            <h5 className='text-[15px] uppercase text-color-lavendar font-barlow-condensed leading-[17px] tracking-[2.3px]'>
+              Est. Travel Time
+            </h5>
+            <h4 className='text-[28px] text-white font-bellefair uppercase leading-[32px]'>
+              {destination.travel}
+            </h4>
+          </div>
+        </div>
       </div>
     </div>
   );
