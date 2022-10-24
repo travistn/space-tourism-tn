cc56a239a1b0a1c0c5228467c8860b502959fb11
travistn <travisn714@gmail.com>
2022-10-24 16:35:48 -0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
         'home-desktop': "url('../src/assets/home/background-home-desktop.jpg')",
         'home-tablet': "url('../src/assets/home/background-home-tablet.jpg')",
         'home-mobile': "url('../src/assets/home/background-home-mobile.jpg')",
+        'destination-mobile': 'url(../src/assets/destination/background-destination-mobile.jpg)',
+        'destination-tablet': 'url(../src/assets/destination/background-destination-tablet.jpg)',
+        'destination-desktop': 'url(../src/assets/destination/background-destination-desktop.jpg)',
       },
       backgroundColor: {
         'navbar-links': 'rgba(255, 255, 255, 0.04)',
