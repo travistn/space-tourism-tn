b1c808bf790a3ab0dc9ac15a6cdd307cf8da0ca7
travistn <travisn714@gmail.com>
2022-10-24 17:37:44 -0700

    Revised styling

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className='w-screen h-screen bg-home-mobile md:bg-home-tablet lg:bg-home-desktop bg-no-repeat bg-cover bg-center flex flex-col'>
       <Navbar />
       <div className='h-screen flex flex-col lg:flex-row lg:items-center lg:mr-[10rem] lg:mt-[5rem] 2xl:mr-[20rem]'>
-        <div className='flex flex-col items-center lg:items-start m-auto w-[330px] md:w-[450px] mt-12 md:mt-[8rem] 2xl:gap-4 '>
+        <div className='flex flex-col items-center lg:items-start m-auto w-[330px] md:w-[450px] mt-12 md:mt-[4rem] 2xl:gap-4 '>
           <h5 className='uppercase text-color-lavendar font-barlow-condensed md:text-[20px] lg:text-[28px] 2xl:text-[32px] tracking-[2.7px] md:tracking-[3.4px] lg:tracking-[4.7px] leading-5 md:leading-6 lg:leading-[34px]'>
             So, you want to travel to
           </h5>
@@ -20,7 +20,7 @@ const Home = () => {
             a truly out of this world experience!
           </p>
         </div>
-        <div className='flex justify-center mt-auto lg:mt-0 2xl:mt-0 mb-12'>
+        <div className='flex justify-center mb-auto lg:mt-0 2xl:mt-0'>
           <div className='flex justify-center items-center w-[150px] md:w-[242px] lg:w-[274px] 2xl:w-[300px] h-[150px] md:h-[242px] lg:h-[274px] 2xl:h-[300px] bg-white rounded-full'>
             <p className='uppercase font-bellefair text-xl md:text-[32px] 2xl:text-[40px] tracking-wider md:tracking-[2px] md:leading-[37px]'>
               Explore
