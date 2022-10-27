ea437070b0ff1d16eb85748136fc7c1706dad204
travistn <travisn714@gmail.com>
2022-10-26 17:19:30 -0700

    Added tablet view for the crew page

--- src/containers/Crew.jsx
+++ src/containers/Crew.jsx
@@ -16,8 +16,8 @@ const Crew = () => {
   return (
     <div className='w-screen min-h-screen bg-crew-mobile bg-no-repeat bg-cover bg-center flex flex-col'>
       <Navbar />
-      <div className='flex flex-col items-center mt-8 mb-4 gap-8'>
-        <h5 className='text-[16px] text-white font-barlow-condensed uppercase tracking-[2.7px] leading-[19px] flex flex-row gap-3'>
+      <div className='flex flex-col items-center mt-8 gap-8'>
+        <h5 className='text-[16px] md:text-[20px] text-white font-barlow-condensed uppercase tracking-[2.7px] md:tracking-[3.4px] leading-[19px] md:leading-[24px] flex flex-row gap-3 md:mr-auto md:ml-8 md:mb-8 md:mt-8'>
           <span className='font-bold opacity-25'>02</span> Meet your crew
         </h5>
         <img
@@ -26,27 +26,31 @@ const Crew = () => {
             .join('-')
             .toLowerCase()}.png`)}
           alt='crew-pic'
-          className='h-[222px]'
+          className='h-[222px] md:h-[572px] md:order-last'
         />
-        <div className='h-[1px] w-[87%] bg-white opacity-20 mt-[-2rem]' />
-        <div className='flex flex-col w-[87%] items-center gap-4'>
-          <div className='flex flex-row gap-4 mb-6'>
+        <div className='h-[1px] w-[87%] bg-white opacity-20 mt-[-2rem] md:hidden' />
+        <div className='flex flex-col w-[87%] md:w-[460px] items-center gap-4'>
+          <div className='flex flex-row gap-4 mb-6 md:order-last md:mt-4'>
             {crew.map((crew) => (
               <div
                 id={crew.name}
                 key={crew.name}
                 onClick={getCrewMember}
-                className={`w-[10px] h-[10px] bg-white rounded-full
+                className={`w-[12px] h-[12px] bg-white rounded-full
                   ${crew === crewMember ? '' : 'opacity-[0.17]'}
                 `}
               />
             ))}
           </div>
           <div className='flex flex-col items-center gap-2 font-bellefair uppercase text-white font-normal'>
-            <h4 className='opacity-50 text-[16px] leading-[18px]'>{crewMember.role}</h4>
-            <h4 className='text-[24px] leading-[28px]'>{crewMember.name}</h4>
+            <h4 className='opacity-50 text-[16px] md:text-[24px] leading-[18px] md:leading-[28px]'>
+              {crewMember.role}
+            </h4>
+            <h4 className='text-[24px] md:text-[40px] leading-[28px] md:leading-[46px]'>
+              {crewMember.name}
+            </h4>
           </div>
-          <p className='w-content font-barlow text-[15px] text-center text-color-lavendar'>
+          <p className='w-content md:h-[90px] font-barlow text-[15px] md:text-[16px] text-center text-color-lavendar'>
             {crewMember.bio}
           </p>
         </div>
